16570b628942570fa1d388053c36d46a355cb611
kskenandore <keiraskenandore@gmail.com>
2018-03-07 14:00:00 -0600

    Created employee class

--- src/transport/Employee.java
+++ src/transport/Employee.java
@@ -1,5 +1,36 @@
 package transport;
 
 public class Employee {
-
+	private String _fname;
+	private String _lname;
+	private String _department;
+	private int _phone;
+	
+	public Employee(String fname, String lname, String department, int phone){
+		_fname = fname;
+		_lname = lname;
+		_department = department;
+		_phone = phone;
+	}
+	
+	public void setFname(String firstname){
+		_fname = firstname;
+	}
+	
+	public void setLname(String lastname){
+		_lname = lastname;
+	}
+	
+	public void setDepartment(String department){
+		_department = department;
+	}
+	
+	public void setPhone(int phone){
+		_phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return _lname + ", " + _fname + " " + _phone + " " + _department;
+	}
 }

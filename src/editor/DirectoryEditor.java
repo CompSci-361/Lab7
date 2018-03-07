e89a3b0677b2fe4e6ffb1f23734ad8317f23281c
adamdunn <adamdunn@uwm.edu>
2018-03-07 14:09:10 -0600

    Start of Client Branch

--- src/editor/DirectoryEditor.java
+++ src/editor/DirectoryEditor.java
@@ -1,12 +1,21 @@
 package editor;
 
 import transport.DirectoryTransport;
+import transport.Employee;
 
 public class DirectoryEditor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		DirectoryTransport transport = new DirectoryTransport();
+		//Read in commands and keep sending new Employees to the proxy
 	
 	}
+	public void addPerson(Employee send) {
+		
+	}
+	public void clear() {
+		
+	}
+	
 }

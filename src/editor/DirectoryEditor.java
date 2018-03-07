93a05b62a8f1f6f92acb31a0d30ecdc7657edbbf
Amrykid <Amrykid@gmail.com>
2018-03-07 13:59:51 -0600

    Main was moved into Dictionary Editor.

--- src/editor/DirectoryEditor.java
+++ src/editor/DirectoryEditor.java
@@ -1,5 +1,12 @@
 package editor;
 
-public class DirectoryEditor {
+import transport.DirectoryTransport;
 
+public class DirectoryEditor {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DirectoryTransport transport = new DirectoryTransport();
+	
+	}
 }
